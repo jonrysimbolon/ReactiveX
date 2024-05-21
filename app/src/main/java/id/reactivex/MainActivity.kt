305b1bcd1bd4413114c7be5f844b6c7913c02585
jonrysimbolon <jonrysimbolon19@gmail.com>
2024-05-21 22:58:28 +0700

    Disposable & CompositeDisposable

--- app/src/main/java/id/reactivex/MainActivity.kt
+++ app/src/main/java/id/reactivex/MainActivity.kt
@@ -1,6 +1,5 @@
 package id.reactivex
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import id.reactivex.databinding.ActivityMainBinding
@@ -8,7 +7,9 @@ import io.reactivex.Flowable
 import io.reactivex.Observable
 import io.reactivex.Scheduler
 import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
+import java.lang.Thread.currentThread
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,22 +17,42 @@ class MainActivity : AppCompatActivity() {
         ActivityMainBinding.inflate(layoutInflater)
     }
 
-    @SuppressLint("CheckResult")
+    private val disposables = CompositeDisposable()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        getEmployeeNames()
-            .subscribeOn(Schedulers.io()) //mengirim data dilakukan dimana
-            .observeOn(AndroidSchedulers.mainThread()) //menerima data dilakukan dimana
-            .subscribe{
-                //println(it)
-                binding.hasilTxt.text = it
+        val disposable1 = Observable.just("1", "2", "3", "4", "5", "6")
+            .map { string -> string.toInt() }
+            .filter { number -> number % 2 == 1 }
+            .doOnNext { println("$it adalah bilangan ganjil") }
+            .count()
+            .subscribe { result -> binding.hasil1Txt.text = "Total bilangan ganjil : $result" }
+
+        val disposable2 = getEmployeeNames()
+            .subscribeOn(Schedulers.io())
+            .doOnNext{ println("emitting in ${currentThread().name}") }
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe { name ->
+                binding.hasil2Txt.text = "$name (consuming in ${currentThread().name})"
             }
+
+        disposables.addAll(disposable1, disposable2)
     }
 
     private fun getEmployeeNames(): Flowable<String> {
         val name = mutableListOf("Buchori", "Dimas", "Tia", "Gilang", "Widy")
-        return  Flowable.fromIterable(name) // untuk membuat Observable dari Array
+        return Flowable.fromIterable(name)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        disposables.clear()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        disposables.dispose()
     }
 }
